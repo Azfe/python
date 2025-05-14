c26ce1c31c7995eb6e95481c2b90fae03b1904eb
Alex Zapata <alexzapata1984@gmail.com>
2025-05-14 12:34:36 +0200

    Add Exercises: Hello World & Class 3

---
+++ class03.py
@@ -0,0 +1,17 @@
+print("Hello,")
+print("World!")
+# This is a comment
+# To comment: Ctrl + K C
+# To discomment: Ctrl + K U
+
+name = "John Doe"
+last_name = "Smith"
+print(name + " " + last_name)
+print(name, last_name)
+
+# print( name + 5)
+print( name * 5)
+
+# Length of string
+print(len(name))
+print(len(last_name))
